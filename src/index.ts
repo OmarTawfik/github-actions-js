20726c0251f23afa608e603e3beb237220f1d5b2
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2019-02-17 23:20:37 -0800

    feat: added scanner + test (#1)

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/*!
+ * Copyright 2019 Omar Tawfik. Please see LICENSE file at the root of this repository.
+ */
+
+export { Compilation } from "./util/compilation";
+export { Token, TextPosition, TextRange, TokenKind } from "./scanning/tokens";
+export { DiagnosticCode, Diagnostic } from "./util/diagnostics";
--- src/scanning/tokens.ts
+++ src/scanning/tokens.ts
@@ -2,7 +2,7 @@
  * Copyright 2019 Omar Tawfik. Please see LICENSE file at the root of this repository.
  */
 
-export const enum TokenType {
+export const enum TokenKind {
   // Top-level keywords
   VersionKeyword = 1,
   WorkflowKeyword = 2,
@@ -41,12 +41,74 @@ export const enum TokenType {
   Unrecognized = 23,
 }
 
-export interface Token {
-  readonly length: number;
-  readonly position: number;
-  readonly type: TokenType;
+export function getTokenDescription(kind: TokenKind): string {
+  switch (kind) {
+    case TokenKind.VersionKeyword:
+      return "version";
+    case TokenKind.WorkflowKeyword:
+      return "workflow";
+    case TokenKind.ActionKeyword:
+      return "action";
+
+    case TokenKind.OnKeyword:
+      return "on";
+    case TokenKind.ResolvesKeyword:
+      return "resolves";
+    case TokenKind.UsesKeyword:
+      return "uses";
+    case TokenKind.NeedsKeyword:
+      return "needs";
+    case TokenKind.RunsKeyword:
+      return "runs";
+    case TokenKind.ArgsKeyword:
+      return "args";
+    case TokenKind.EnvKeyword:
+      return "env";
+    case TokenKind.SecretsKeyword:
+      return "secrets";
+
+    case TokenKind.Equal:
+      return "=";
+    case TokenKind.Comma:
+      return ",";
+
+    case TokenKind.LeftCurlyBracket:
+      return "{";
+    case TokenKind.RightCurlyBracket:
+      return "}";
+    case TokenKind.LeftSquareBracket:
+      return "[";
+    case TokenKind.RightSquareBracket:
+      return "]";
+
+    case TokenKind.Identifier:
+      return "identifier";
+    case TokenKind.Comment:
+      return "comment";
+
+    case TokenKind.IntegerLiteral:
+      return "integer";
+    case TokenKind.StringLiteral:
+      return "string";
+
+    case TokenKind.Missing:
+      return "missing";
+    case TokenKind.Unrecognized:
+      return "unrecognized";
+  }
 }
 
-export namespace TokenUtils {
-  export const getContents = (text: string, token: Token): string => text.substr(token.position, token.length);
+export interface TextPosition {
+  readonly line: number;
+  readonly column: number;
+}
+
+export interface TextRange {
+  readonly start: TextPosition;
+  readonly end: TextPosition;
+}
+
+export interface Token {
+  readonly kind: TokenKind;
+  readonly range: TextRange;
 }
