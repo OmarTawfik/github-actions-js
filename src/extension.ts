07a3a087d36c43ae84c4177f3a9ba015a51c1df5
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2019-03-03 22:07:00 -0800

    Added vscode extension with highlighting and snippets (#15)

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+/*!
+ * Copyright 2019 Omar Tawfik. Please see LICENSE file at the root of this repository.
+ */
+
+import * as vscode from "vscode";
+
+export function activate(_: vscode.ExtensionContext): void {}
+
+export function deactivate(): void {}
