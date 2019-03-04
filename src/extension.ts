003f3e53d6a785374e0f46d6af07760c5a7095ea
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2019-03-04 01:20:22 -0800

    Added LSP + diagnostics service (#23)

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,39 @@
  * Copyright 2019 Omar Tawfik. Please see LICENSE file at the root of this repository.
  */
 
-import * as vscode from "vscode";
+import * as path from "path";
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from "vscode-languageclient";
 
-export function activate(_: vscode.ExtensionContext): void {}
+const LANGUAGE_NAME = "github-actions";
 
-export function deactivate(): void {}
+let client: LanguageClient | undefined;
+
+export function activate(): void {
+  const runOptions = {
+    module: path.join(__dirname, "server.js"),
+    transport: TransportKind.ipc,
+  };
+
+  const debugOptions = {
+    ...runOptions,
+    options: { execArgv: ["--nolazy", "--inspect=6009"] },
+  };
+
+  const serverOptions: ServerOptions = {
+    run: runOptions,
+    debug: debugOptions,
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: LANGUAGE_NAME }],
+  };
+
+  client = new LanguageClient(LANGUAGE_NAME, `${LANGUAGE_NAME} client`, serverOptions, clientOptions);
+  client.start();
+}
+
+export async function deactivate(): Promise<void> {
+  if (client) {
+    await client.stop();
+  }
+}
