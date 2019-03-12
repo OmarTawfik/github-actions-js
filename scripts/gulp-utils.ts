918de95cf94e203038857927a4ea31586e066300
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2019-03-12 01:02:19 -0700

    fix: remove scripts from vscode extension before publishing

--- scripts/gulp-utils.ts
+++ scripts/gulp-utils.ts
@@ -5,10 +5,29 @@
 import * as path from "path";
 import * as gulp from "gulp";
 import * as shell from "gulp-shell";
+import * as merge from "gulp-merge-json";
 
 export const rootPath = path.resolve(__dirname, "..");
 export const outPath = path.join(rootPath, "out");
 
-export function gulp_shell(name: string, factory: () => string[]): void {
-  gulp.task(name, shell.task(factory().join(" ")));
+export function gulp_shell(taskName: string, factory: () => string[]): void {
+  gulp.task(taskName, shell.task(factory().join(" ")));
+}
+
+export function gulp_mergePackageJson(taskName: string, specificPackage: string, outputFolder: string): void {
+  gulp.task(taskName, () => {
+    return gulp
+      .src([path.join(rootPath, "package.json"), path.join(rootPath, "scripts", specificPackage)])
+      .pipe(
+        merge({
+          fileName: "package.json",
+          edit: contents => {
+            contents.scripts = {}; // remove vscode postinstall step, as it is only needed during dev mode
+            contents.devDependencies = {}; // Remove devDependencies to improve publish step speed
+            return contents;
+          },
+        }),
+      )
+      .pipe(gulp.dest(outputFolder));
+  });
 }
